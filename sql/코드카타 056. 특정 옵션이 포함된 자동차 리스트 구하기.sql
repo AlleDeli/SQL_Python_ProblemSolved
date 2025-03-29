a9a59ce9b7684784aa201a415895250a384510f9
AlleDeli <157803895+AlleDeli@users.noreply.github.com>
2025-03-29 21:00:20 +0900

    Update and rename 코드카타 056. 문제제목.sql to 코드카타 056. 특정 옵션이 포함된 자동차 리스트 구하기.sql

---
+++ sql/코드카타 056. 특정 옵션이 포함된 자동차 리스트 구하기.sql
@@ -0,0 +1,9 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/157343
+
+SELECT car_id,
+       car_type,
+       daily_fee,
+       options
+FROM car_rental_company_car
+WHERE options like '%네비게이션%'
+ORDER BY car_id desc
