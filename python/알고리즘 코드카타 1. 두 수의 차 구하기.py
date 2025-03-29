18ca831ce83888937ce243472cc95f72e4b4d075
AlleDeli <157803895+AlleDeli@users.noreply.github.com>
2025-03-29 19:46:06 +0900

    Create 알고리즘 코드카타 1. 두 수의 차 구하기.py

---
+++ python/알고리즘 코드카타 1. 두 수의 차 구하기.py
@@ -0,0 +1,7 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120803
+
+def solution(num1, num2):
+    if -50000 <= num1 <= 50000 and -50000 <= num2 <= 50000:
+        return num1 - num2
+    else:
+        print('error!')
