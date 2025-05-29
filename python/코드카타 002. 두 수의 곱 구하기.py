fb780d2fa7d7a008d965787bc3fb9daf6db810c0
AlleDeli <157803895+AlleDeli@users.noreply.github.com>
2025-05-29 18:18:23 +0900

    Create 코드카타 002. 두 수의 곱 구하기.py

---
+++ python/코드카타 002. 두 수의 곱 구하기.py
@@ -0,0 +1,7 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120804
+
+def solution(num1, num2):
+    if 0 <= num1 <= 100 and 0 <= num2 <= 100:
+        return num1 * num2
+    else:
+        print('error!')
